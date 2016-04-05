cdf1403653c1f23fff506df628da227c1b313eb9
stella <stpsomad@gmail.com>
2016-04-05 13:30:23 +0200

    updating file

--- pointcloud/duplicates/duplicate.py
+++ pointcloud/duplicates/duplicate.py
@@ -4,6 +4,8 @@ Created on Sun Jan 03 20:20 2016
 
 @author: Stella Psomadaki
 
+Command line executable to remove duplicates. Only one folder per run.
+
 This module takes a laz, las file and removes duplicate points in the 
 (x,y) dimensions. This is an important preprocessing step because the IOT
 does not allow duplicates in the index.
@@ -11,35 +13,32 @@ does not allow duplicates in the index.
 
 import numpy as np
 from pandas import DataFrame
-import time 
+import time
 from laspy.file import File
 import os
 from pointcloud.reader import readFileLaspy
-from pointcloud.general import getFiles
+from pointcloud.utils import getFiles
+import sys, getopt
 
-
-def removeDuplicate(cfile):
+def removeDuplicate(file):
     """Removes duplicate points based on X, Y coordinates
 
        Returns a numpy array"""
-    df = DataFrame(np.vstack((cfile.x, cfile.y, cfile.z)).transpose(), columns=['X', 'Y', 'Z'])
-    df.drop_duplicates(subset=['X', 'Y'], inplace=True)
+    df = DataFrame(np.vstack((file.x, file.y, file.z)).transpose(), columns=['X', 'Y', 'Z'])
+    df.drop_duplicates(subset=['X','Y'], inplace=True)
     return df.values
 
-
-def writeFile(directory, name, header, coords):
+def writeFile(directory,name,header,coords):
     """Write a laz file using laspy and numpy arrays"""
-    output = File(directory + name, mode="w", header=header)
+    output = File(directory + name, mode = "w", header=header)
     output.x = coords[0]
     output.y = coords[1]
     output.z = coords[2]
     output.close()
 
-
 def checkDirectory(directory):
-    """ Checks if the specified directory exists,
-    and otherwise it creates it"""
-    try:
+    """ Checks if the specified directory exists, and otherwise it creates it"""
+    try: 
         os.makedirs(directory)
     except OSError:
         if not os.path.isdir(directory):
@@ -49,20 +48,34 @@ def lasDuplicateFree(directory, output):
     """ Takes a directory with las [laz] files and an output directory
 
         Returns las, [laz] files free from duplicates"""
-    files = getFiles(directory,['laz'], False)
+    files = getFiles(directory,['laz'],True)
     checkDirectory(output)
-    for cfile in files:
-        print cfile
-        fh = readFileLaspy(cfile)
-        writeFile(output,cfile[cfile.rfind('\\')+1:],fh.header,removeDuplicate(fh).transpose())
+    for file in files:
+        print file
+        fh = readFileLaspy(file)
+        writeFile(output,file[file.rfind('\\')+1:],fh.header,removeDuplicate(fh).transpose())
+
+def main(argv):
+    inputdir = ''
+    outputdir = ''
+    try:                                
+        opts, args = getopt.getopt(argv, "hi:o:", ["help", "input=", "output="])
+    except getopt.GetoptError:
+        print 'lasduplicate.py -i <inputDirectory> -o <outputDirectory>'
+        sys.exit(2)                     
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print argv[0] +' -i <inputDirectory> -o <outputDirectory>'
+            sys.exit()                  
+        elif opt in ("-i", "--input"):
+            inputdir = arg            
+        elif opt in ("-o", "--output"):
+            outputdir = arg
+    lasDuplicateFree(inputdir, outputdir)
 
 if __name__ =="__main__":
-    paths  = ['D:/Dropbox/Thesis/Thesis/Data/from2016/']
-    pathOutput = 'D:/Dropbox/Thesis/Thesis/Data/Cleaned_datasets/Zandmotor_bench/'
-    outputs = [pathOutput + 'noise/']
-    
     start = time.time()
-    for path in range(len(paths)):
-        lasDuplicateFree(paths[path], outputs[path])
+    main(sys.argv[1:])
     end = time.time()
-    print "Finished within {0} seconds".format(end - start)
+    print "Finished in ", end - start
+    #Example run: python duplicate.py -i D:\ -o D:\output\
